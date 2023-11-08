cbfe249350626eca94df6ed7b6810bc8c545ae6c
gae-ddong <144202407+gae-ddong@users.noreply.github.com>
2023-11-08 19:55:42 +0900

    [Silver IV] Title: 수 찾기, Time: 424 ms, Memory: 46712 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,31 @@
+# 암기왕
+
+#T = int(input())
+
+
+def binarySearch(note, a):
+    start = 0
+    end = len(note) - 1
+
+    while start <= end:
+        mid = (start+end)//2
+
+        if note[mid] == a:
+            return 1
+        elif note[mid] < a:
+            start = mid + 1
+        else:
+            end = mid - 1
+
+    return 0
+
+
+#for _ in range(T):
+N = int(input())
+note1 = list(map(int, input().split()))
+M = int(input())
+note2 = list(map(int, input().split()))
+
+note1.sort()
+for i in note2:
+    print(binarySearch(note1, i))
