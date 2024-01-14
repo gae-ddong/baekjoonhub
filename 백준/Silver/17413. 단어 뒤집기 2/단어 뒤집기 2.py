c1a0be02ad3128056886cc45a8d05f8053441643
gae-ddong <144202407+gae-ddong@users.noreply.github.com>
2024-01-14 13:48:57 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 104 ms, Memory: 32684 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
@@ -4,29 +4,27 @@ word = list(input())
 
 reverse = 1  # <>안에 있으면 0, <> 안에 없으면 1
 rev_list = []  # 역정렬할 부분 리스트
-ans = []  # 정답
 
 for i in word:
     if i == '<':
         reverse = 0
         if len(rev_list) != 0:
             rev_list.reverse()
-            ans = ans + rev_list
+            print(''.join(rev_list), end="")
             rev_list = []
-        ans.append(i)
+        print(i, end="")
     elif i == '>':
         reverse = 1
-        ans.append(i)
+        print(i, end="")
     elif reverse == 1:
         if i == ' ':
             rev_list.reverse()
-            ans = ans + rev_list
-            ans.append(i)
+            print(''.join(rev_list), end="")
             rev_list = []
+            print(i, end="")
         else:
             rev_list.append(i)
     elif reverse == 0:
-        ans.append(i)
+        print(i, end="")
 rev_list.reverse()
-ans = ans + rev_list
-print(''.join(ans))
+print(''.join(rev_list), end="")
